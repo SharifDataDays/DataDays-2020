ac54f28fa0ccb94b992a7e4b06a487091cff7b94
Rez <reza.amini.majd1379@gmail.com>
2019-12-24 17:28:27 +0330

    get score function

--- apps/contest/services/score.py
+++ apps/contest/services/score.py
@@ -1,6 +1,38 @@
+import ast
+
+from apps.contest.models import ScoreStatusTypes
+from apps.question.models import QuestionTypes
 
 
 class Score:
 
-    def get_score(self,question_submission):
-        pass
+    def get_score(self, question_submission):
+        question = question_submission.question
+        answer = question_submission.answer
+        answer = ast.literal_eval(answer)
+
+        def get_path():
+            return question.dir_path
+
+        try:
+            exec(question.judge_function)
+            function_name = question.judge_function_name
+            answer_name, answer = self.get_parameters(question.type, answer)
+            call_function = f'{function_name}({answer_name}={answer})'
+            score = exec(call_function)
+            score.save()
+        except JudgeException as e:
+            score.status = ScoreStatusTypes.FAILED
+            score.info = str(e)
+            score.save()
+        except Exception:
+            score.status = ScoreStatusTypes.ERROR
+            score.save()
+
+    def get_parameters(self, question_type, answer):
+        if question_type in [QuestionTypes.SINGLE_ANSWER, QuestionTypes.SINGLE_SELECT]:
+            return 'answer', answer[0]
+        elif question_type in [QuestionTypes.NUMERIC_RANGE, QuestionTypes.MULTI_ANSWER, QuestionTypes.MULTI_SELECT]:
+            return 'answers', answer
+        elif question_type == QuestionTypes.FILE_UPLOAD:
+            return 'file_path', answer[0]
