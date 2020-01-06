bbe1d0c242d3f955c619b00c81422e8cb44b0231
alireza <alirezashateri7@gmail.com>
2020-01-06 19:55:51 +0330

    Debug contest app url patterns

--- apps/contest/urls.py
+++ apps/contest/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'contest'
 
 urlpatterns = [
-    path('contests/', views.ContestsListAPIView, name='contests_list'),
+    path('contests/', views.ContestsListAPIView.as_view(), name='contests_list'),
     path('<int:contest_id>/', views.ContestAPIView.as_view(), name='contest'),
     path('<int:contest_id>/<int:milestone_id>/', views.MilestoneAPIView.as_view(), name='milestone'),
     path('<int:contest_id>/<int:milestone_id>/<int:task_id>/', views.CreateTrialAPIView.as_view(), name='create_trial'),
