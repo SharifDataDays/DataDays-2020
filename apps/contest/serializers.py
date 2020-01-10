9fe2ad152bd4ababdbe8ecd4b8504e0db0ead929
mrtaalebi <the.doors.are.locked@gmail.com>
2020-01-10 19:26:48 +0330

    Fix bug

--- apps/contest/serializers.py
+++ apps/contest/serializers.py
@@ -56,7 +56,7 @@ class QuestionSubmissionSerializer(ModelSerializer):
         fields = ['id', 'question', 'answer', 'score']
 
     def get_answer(self, obj):
-        if obj.question.type == QuestionTypes.FILE_UPLOAD and len(obj.answer) == 1:
+        if obj.question.type == QuestionTypes.FILE_UPLOAD and len(eval(obj.answer)) == 1:
                 return ['/media' + obj.answer[0]]
         return obj.answer
 
