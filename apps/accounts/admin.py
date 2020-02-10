101ee689ee0ad443ca4bc73ca29ee63fad47b78f
mrtaalebi <the.doors.are.locked@gmail.com>
2020-02-10 22:49:02 +0330

    Add export to csv to profile

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,9 +1,34 @@
+import csv
+
+from django.http import HttpResponse
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.utils.http import urlsafe_base64_decode
 
 from apps.accounts.models import \
     University, Profile, ResetPasswordToken, ActivateUserToken
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = \
+            'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow(
+                [getattr(obj, field) for field in field_names]
+            )
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 @admin.register(University)
@@ -13,7 +38,7 @@ class UniversityAdmin(admin.ModelAdmin):
 
 
 @admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(admin.ModelAdmin, ExportCsvMixin):
     search_fields = [
         'firstname_fa',
         'firstname_en',
@@ -34,6 +59,8 @@ class ProfileAdmin(admin.ModelAdmin):
         'major',
         'bmp',
     ]
+
+    actions = ['export_as_csv']
 
 
 @admin.register(ResetPasswordToken)
