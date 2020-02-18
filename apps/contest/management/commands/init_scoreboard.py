94b63c42ffe303c0bf56bd96fa132e3e9e84d5ac
mrtaalebi <the.doors.are.locked@gmail.com>
2020-02-19 02:35:02 +0330

    Add sleep

--- apps/contest/management/commands/init_scoreboard.py
+++ apps/contest/management/commands/init_scoreboard.py
@@ -1,10 +1,6 @@
-import requests
-import os
+from time import sleep
 
 from django.core.management.base import BaseCommand
-from django.conf import settings
-
-from rest_framework import status
 
 from apps.contest import models as contest_models
 from apps.contest.services.scoreboard import Scoreboard
@@ -68,19 +64,23 @@ class Command(BaseCommand):
                     self._add_task(task)
                     self._add_task_to_milestone(task, milestone)
                     self._add_task_to_milestone(task, contest)
+                    sleep(0.5)
 
     def _handle_add_task_to_milestone(self, options):
         if not options.get('ids') or len(options.get('ids')) != 2:
-            print("Please Enter Command Like This: --add_task_to_milestone {task_id} {milestone_id}")
+            print("Please Enter Command Like This: "
+                  "--add_task_to_milestone <task_id> <milestone_id>")
         else:
-            task_id, milestone_id = options.get('ids')[0], options.get('ids')[1]
+            task_id, milestone_id = \
+                options.get('ids')[0], options.get('ids')[1]
             try:
                 task = contest_models.Task.objects.get(id=task_id)
             except contest_models.Task.DoesNotExist as e:
                 task = None
                 print(e)
             try:
-                milestone = contest_models.Milestone.objects.get(id=milestone_id)
+                milestone = contest_models.Milestone.objects.get(
+                    id=milestone_id)
             except contest_models.Milestone.DoesNotExist as e:
                 milestone = None
                 print(e)
@@ -102,11 +102,13 @@ class Command(BaseCommand):
 
     def _handle_add_milestone(self, options):
         if not options.get('ids') or len(options.get('ids')) != 1:
-            print("Please Enter Command Like This: --add_milestone {milestone_id}")
+            print("Please Enter Command Like This: "
+                  "--add_milestone <milestone_id>")
         else:
             milestone_id = options.get('ids')[0]
             try:
-                milestone = contest_models.Milestone.objects.get(id=milestone_id)
+                milestone = contest_models.Milestone.objects.get(
+                    id=milestone_id)
             except contest_models.Milestone.DoesNotExist as e:
                 milestone = None
                 print(e)
@@ -119,5 +121,6 @@ class Command(BaseCommand):
     def _add_task(self, task: contest_models.Task) -> None:
         Scoreboard.add_task(task)
 
-    def _add_task_to_milestone(self, task: contest_models.Task, milestone: contest_models.Milestone) -> None:
+    def _add_task_to_milestone(self, task: contest_models.Task,
+                               milestone: contest_models.Milestone) -> None:
         Scoreboard.add_task_to_milestone(task, milestone)
