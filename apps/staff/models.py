168797efb5b64985e497b05a8f5b522e8cccd2b5
mrtaalebi <the.doors.are.locked@gmail.com>
2020-02-24 16:30:49 +0330

    Add order to subteam

--- apps/staff/models.py
+++ apps/staff/models.py
@@ -15,6 +15,7 @@ class SubTeam(models.Model):
     title_fa = models.CharField(max_length=50)
     team = models.ForeignKey(
         Team, on_delete=models.CASCADE, related_name='subteams',null=True)
+    order = models.IntegerField(default=0)
 
     def __str__(self):
         return '%s' % (self.title_fa)
