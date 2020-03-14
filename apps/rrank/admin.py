2b0c33f9c40170f44d21d81f6e133f818260731a
mrtaalebi <the.doors.are.locked@gmail.com>
2020-03-15 00:56:56 +0330

    Add TeamNum admin

--- apps/rrank/admin.py
+++ apps/rrank/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from apps.rrank.models import TeamNum
+
+
+@admin.register(TeamNum)
+class TeamNumAdmin(ModelAdmin):
+    list_display = ['team', 'num']
+    sortable_by = ['team', 'num']
+    readonly_fields = ['team', 'num']
